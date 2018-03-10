767d17ffa01d3c49fa37961ffc5b743cfe0dbe23
365556520 <33852383+365556520@users.noreply.github.com>
2018-03-10 20:01:26 +0800

    上传图像没做好

--- app/Http/Model/User.php
+++ app/Http/Model/User.php
@@ -11,4 +11,7 @@ class User extends Model
     protected $primaryKey='user_id';
     //设为假的话，表示create方法执行时，不会对create_at和updated_at修改
 //    public $timestamps=false;
+    protected $fillable = [
+        'user_name','user_pass',
+    ];
 }
