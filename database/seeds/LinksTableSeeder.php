9280088d7edf15aad67951d6abca8202069adc74
365556520 <33852383+365556520@users.noreply.github.com>
2018-03-10 19:58:40 +0800

    上传图像没做好

--- database/seeds/LinksTableSeeder.php
+++ database/seeds/LinksTableSeeder.php
@@ -26,5 +26,19 @@ class LinksTableSeeder extends Seeder
             ]
         ];
         DB::table('links')->insert($data);
+        $admin= [
+            [
+                'link_name' => '百度',
+                'link_title' => '国内称霸搜索引擎',
+                'link_url' => 'http://www.baidu.com',
+                'link_order' => 1,
+            ],
+            [
+                'user_name' => 'aaa',
+                'user_pass' => bcrypt('aaaaaa'),
+            ]
+        ];
+        DB::table('user')->insert($admin);
+
     }
 }
